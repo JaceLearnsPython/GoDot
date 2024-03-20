4c09c814ed028214199c0401cf936e82369b24a5
JaceLearnsPython <jtdenton14@gmail.com>
2024-03-20 15:21:16 -0400

    Testing. Updated Scene for SinglePlayer

--- card_ui/card_ui.gd
+++ card_ui/card_ui.gd
@@ -1,11 +1,13 @@
 extends Control
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var cardSize = size
 	$Area2D/CardCollision.scale = cardSize
-
+	
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- scenes/SinglePlayer.gd
+++ scenes/SinglePlayer.gd
@@ -11,14 +11,15 @@ const singePlayerScene = preload("res://scenes/SinglePlayer.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	deal(1) # give the number of cards to deal
+	var card = load("res://ass/cards/card_clubs_03.png")
+	print(card)
+	print($CardTest)
+	$CardTest.set_texture(card)
 	pass # Replace with function body.
-	
-func deal(num) -> void:
-	pass # need to deal cards
 
 func shuffle():
 	pass
+
 #
 func _physics_process(_delta: float) -> void:
 	pass # nothing yet
