bf9157bd1b6062462db359ef16bbb6082e104f56
JaceLearnsPython <jtdenton14@gmail.com>
2024-04-15 13:33:06 -0400

    Implemented playing ability and logic for playing cards. Ascending order and must match suit

--- card_ui/card_ui.gd
+++ card_ui/card_ui.gd
@@ -6,19 +6,41 @@ var in_droppable
 var og_pos: Vector2
 var area_node
 var card_num
+var played = false
 
 
 func _ready():
 	og_pos = position
 
 func _process(delta):
-	if in_droppable and not selected:
-		card_num = str(area_node)
-		print(card_num)
+	if in_droppable and not selected and $TextureRect.texture != null:
+		card_played()
+			
+func card_played():
+		card_num = str($TextureRect.texture.get_path())
+		card_num = card_num.split("_")
 		
+		# check if it is an empty stack
 		if area_node.texture == null:
-			# check if an Ace!
-			area_node.texture = $TextureRect.texture
+			if card_num[2].contains("1"):
+				area_node.texture = $TextureRect.texture
+				played = true
+
+				
+		# otherwise there is a card
+		if area_node.texture != null:
+			var node_str = str(area_node.texture.get_path())
+			node_str = node_str.split("_") # split it so its [0]=path, [1] = suit [2] = num.png
+			# check if we can play it 
+			var num = card_num[2].split(".")
+			
+			var node_num = int(node_str[2].split(".")[0])
+			num = int(num[0]) # get it as an int
+			
+			if num == (node_num+1) and node_str[1] == card_num[1]:
+				area_node.texture = $TextureRect.texture
+				played = true
+
 
 # handle input for card dragging
 func _input(event):
@@ -32,14 +54,12 @@ func _input(event):
 	if Input.is_action_just_released("left-click"):
 		selected = false
 		position = og_pos
-
 	
 func _on_area_2d_mouse_entered():
 	mouse_in = true
 
 func _on_area_2d_mouse_exited():
 	mouse_in = false
-
 
 func _on_area_2d_area_entered(area):
 	
@@ -52,6 +72,10 @@ func _on_area_2d_area_entered(area):
 	
 	in_droppable = true
 
-
 func _on_area_2d_area_exited(area):
 	in_droppable = false
+
+	
+	
+	
+	
