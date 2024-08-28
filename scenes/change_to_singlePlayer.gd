c10b5fc554fe3f05fd1641a5c887e5c765d9f2d5
JaceLearnsPython <jtdenton14@gmail.com>
2024-08-28 14:04:16 -0400

    Updated cards, main menu, and buttons for moving to singleplayer. Investigating the possibility of using a popup menu node instead of 3 mode buttons to save space and make it more intuitive.

--- scenes/change_to_singlePlayer.gd
+++ scenes/change_to_singlePlayer.gd
@@ -1,10 +1,20 @@
 extends Button
 
-# Called when the node enters the scene tree for the first time.
-
-#
-func _physics_process(_delta: float) -> void:
-	pass # nothing yet
-
+# Set Delay, 140 = Easy, 90 = Normal, 50 = Hard 
+# get what mode and change scene to game
+#signal mode_update(delay)
 func _on_pressed():
+	var node = get_node(".")
+
+	if node.to_string().contains("easy"):
+		print("Easy mode!")
+		#ai_Script.delay = 140
+	elif node.to_string().contains("normal"):
+		print("Normal mode!")
+		#ai_Script.delay = 90
+	else:
+		print("Hard Mode!")
+		#ai_Script.delay = 50
+		
+			
 	get_tree().change_scene_to_file("res://scenes/SinglePlayer.tscn")
